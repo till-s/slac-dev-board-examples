3e44f7f67032c1cb89e87f6395efc92d1dee5c29
vivado <strauman@slac.stanford.edu>
2019-04-24 20:37:04 -0500

    Ila_256Pkg.vhd, common/core/ruckus.tcl
    Added trigger i/o to ila IP and also added a package
    with a component declaration that provides default
    values for trigger i/o so that the user doesn't
    *have* to hook these up.

--- firmware/common/core/ruckus.tcl
+++ firmware/common/core/ruckus.tcl
@@ -13,6 +13,8 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 if { [llength [get_ips Ila_256]] == 0 } {
 	create_ip -name ila -vendor xilinx.com -library ip -version 6.2 -module_name Ila_256
 	set_property -dict [list CONFIG.C_PROBE0_WIDTH {64} CONFIG.C_PROBE1_WIDTH {64} CONFIG.C_PROBE2_WIDTH {64} CONFIG.C_PROBE3_WIDTH {64} CONFIG.C_NUM_OF_PROBES {4} ] [get_ips Ila_256]
+	set_property -dict [list CONFIG.C_TRIGOUT_EN {true} CONFIG.C_INPUT_PIPE_STAGES {1} CONFIG.C_TRIGIN_EN {true}] [get_ips Ila_256]
+
 }
 
 # Load Source Code
