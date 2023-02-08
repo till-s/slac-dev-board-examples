d3b49affd4a4d5773fe2635d2470e7397b440b11
vivado <strauman@slac.stanford.edu>
2023-02-08 13:06:56 +0100

    ruckus.tcl: source scripts for generating IP

--- firmware/targets/EcevrProto/ruckus.tcl
+++ firmware/targets/EcevrProto/ruckus.tcl
@@ -4,6 +4,15 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 # Load common and sub-module ruckus.tcl files
 loadRuckusTcl $::env(PROJ_DIR)/../../
 
+# Generate IP
+if { [llength [get_ips Ila_256]] == 0 } {
+	source $::env(PROJ_DIR)/tcl/genIla256.tcl
+}
+
+if { [llength [get_ips TimingGtp]] == 0 } {
+	source $::env(PROJ_DIR)/tcl/genTimingGtp.tcl
+}
+
 # Load local source Code and constraints
 foreach f {
   EcEvrProto.vhd
