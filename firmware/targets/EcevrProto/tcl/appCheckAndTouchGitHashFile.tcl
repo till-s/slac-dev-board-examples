8c5f25c08b719d65050bffe87ef66fc26690d911
till straumann <till.straumann@alumni.tu-berlin.de>
2023-03-14 09:14:45 +0100

    WIP on tcl scripts

--- firmware/targets/EcevrProto/tcl/appCheckAndTouchGitHashFile.tcl
+++ firmware/targets/EcevrProto/tcl/appCheckAndTouchGitHashFile.tcl
@@ -1,11 +1,16 @@
-proc appCheckAndTouchGitHashFile { fn } {
-
+proc getGitHash {} {
   if { [exec sh -c {git update-index && git diff-index --quiet HEAD; echo $?}] != 0 } {
     set git_hash {00000000}
     puts "GIT NOT CLEAN - not providing a hash"
   } else {
     set git_hash "[exec git rev-parse --short=8 HEAD]"
   }
+  return "${git_hash}"
+}
+
+proc appCheckAndTouchGitHashFile { fn } {
+
+  set git_hash [getGitHash]
 
   if { ! [file exists "${fn}"] || ( [exec sh -c "grep -q '${git_hash}' '${fn}'; echo \$?"] != 0 ) } {
     set fp [open "${fn}" w+]
@@ -21,4 +26,6 @@ proc appCheckAndTouchGitHashFile { fn } {
     close $fp
   }
   puts ${git_hash}
+
+  return "${git_hash}"
 }
--- firmware/targets/EcevrProto/vivado/pre_synth_run.tcl
+++ firmware/targets/EcevrProto/vivado/pre_synth_run.tcl
@@ -1,10 +1,14 @@
 puts "USER PRE_SYNTH_RUN script"
 
-source [get_files */appCheckAndTouchGitHashFile.tcl]
+set origin_dir [file dirname [info script]]
 
-appCheckAndTouchGitHashFile [get_files */AppGitHashPkg.vhd]
+# apparently utils_1 fileset is not set up from where we are called
+#source [get_files -of_objects [get_filesets utils_1] {*/appCheckAndTouchGitHashFile.tcl}]
+source "${origin_dir}/../tcl/appCheckAndTouchGitHashFile.tcl"
 
-set genericArgList [get_property generic -objects [current_fileset]]
+set git_hash [appCheckAndTouchGitHashFile "${origin_dir}/../hdl/AppGitHashPkg.vhd"]
+
+set genericArgList [get_property generic [current_fileset]]
 lappend genericArgList "GIT_HASH_G=32'h${git_hash}"
 
-set_property generic ${genericArgList} -objects [current_fileset]
+set_property generic ${genericArgList} [current_fileset]
